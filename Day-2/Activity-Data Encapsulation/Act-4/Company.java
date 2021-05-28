808b2cdcac76f21c5eb2ba77396ea53d67292530
99004474-vinay <84534534+99004474-vinay@users.noreply.github.com>
2021-05-28 11:22:35 +0530

    Create Company.java

---
+++ Day-2/Activity-Data Encapsulation/Act-4/Company.java
@@ -0,0 +1,32 @@
+class Company
+{
+  private String name;
+  private String[] employees;
+  private String teamlead;
+  
+
+  public String getName()
+  {
+    return name;
+  }
+  public void setName(String name)
+  {
+     this.name=name;
+  }
+  public String[] getEmployees()
+  {
+    return employees;
+  }
+  public void setEmployees(String[] employees)
+  {
+    this.employees=employees;
+  }
+  public String getTeamLead()
+  {
+     return teamlead;
+  }
+  public void setTeamLead(String teamlead)
+  {
+    this.teamlead=teamlead;
+  }
+}
