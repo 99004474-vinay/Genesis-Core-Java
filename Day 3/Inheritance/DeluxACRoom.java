6075d22546b5d9ac4324e8f8a2737fa0314fdc0f
99004474-vinay <84534534+99004474-vinay@users.noreply.github.com>
2021-05-31 22:05:32 +0530

    Add files via upload

---
+++ Day 3/Inheritance/DeluxACRoom.java
@@ -0,0 +1,10 @@
+
+public class DeluxACRoom extends DeluxRoom{
+
+	public DeluxACRoom(String hotelName, int noOfSqft, boolean hasTv, boolean hasWifi) {
+		super(hotelName, noOfSqft, hasTv, hasWifi);
+		// TODO Auto-generated constructor stub
+		setRatePerSqft(12);
+	}
+	
+}
