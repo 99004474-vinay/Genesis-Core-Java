427c5235e240693cf7107ea6f9be886c28c2541d
99004474-vinay <84534534+99004474-vinay@users.noreply.github.com>
2021-05-31 22:05:02 +0530

    Add files via upload

---
+++ Day 3/Inheritance/DeluxRoom.java
@@ -0,0 +1,36 @@
+
+public class DeluxRoom extends HotelRoom {
+	private int ratePerSqft;
+	public DeluxRoom(String hotelName, int noOfSqft, boolean hasTv, boolean hasWifi) {
+		// TODO Auto-generated constructor stub
+		super(hotelName,noOfSqft,hasTv,hasWifi);
+		setRatePerSqft(10);
+		
+	}
+	/**
+	 * @return the ratePerSqft
+	 */
+	public int getRatePerSqft() {
+		return ratePerSqft;
+	}
+	/**
+	 * @param ratePerSqft the ratePerSqft to set
+	 */
+	public void setRatePerSqft(int ratePerSqft) {
+		this.ratePerSqft = ratePerSqft;
+		if(this.isHasWifi())
+		{
+			this.ratePerSqft+=2;
+		}
+	}
+	
+	public int getratePerSqft()
+	{
+		return this.ratePerSqft;
+	}
+	public int calculateTarrif()
+	{
+		System.out.println(this.ratePerSqft+" "+this.getNoOfSqft());
+		return this.ratePerSqft*this.getNoOfSqft();
+	}
+}
